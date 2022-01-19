cf46d76533ff4026fd52d9578650b96f7eec7b22
Vasmayer <lealimarco90@gmail.com>
2022-01-19 15:34:48 +0100

    es completed with BONUS

--- js/script.js
+++ js/script.js
@@ -10,6 +10,15 @@ Usiamo il DOM per stampare e chiedere le informazioni all'utente!
 
 */
 
+/* funzione random number */
+
+const rdnNumber = (min,max) => Math.floor(Math.random() * ( max - min )) + min 
+
+
+    
+
+
+/* _______________________________ 1 _______________________________ */
 /* creo l'oggetto studente */
 studente =
 {
@@ -17,10 +26,46 @@ studente =
     cognome : 'Gates',
     eta : 67
 }
+/* _______________________________ 3 _______________________________ */
+studenti = [
+    {
+        nome : 'Bill',
+        cognome : 'Gates',
+        eta : 67
+    },
+    {
+        nome : 'Elon',
+        cognome : 'Musk',
+        eta : 50
+    },
+    {
+        nome : 'Jeff',
+        cognome : 'Besoz',
+        eta : 58
+    },
+    {
+        nome : 'Richard',
+        cognome : 'Branson',
+        eta : 71
+    },
+    {
+        nome : 'Mark',
+        cognome : 'Zuckerberg',
+        eta : 37
+    }
+];
 
+/* prendo gli elementi del DOM che mi servono */
 const propertyElement = document.getElementById('property');
-let message = "<div class='my-5'><h5>Le Propietà sono:</h3><br>";
+const studentsElement = document.getElementById('students');
+const insertElement = document.getElementById('insert');
+const nameElement = document.getElementById('name');
+const surnameElement = document.getElementById('surname');
+const etaElement = document.getElementById('eta');
 
+let message = "<div class='my-5'><h5> 2. Le Propietà sono:</h3><br>";
+/* _______________________________ 2 _______________________________ */
+/* stampo le propietà dell'oggetto */
 for(let key in studente)
 {
     message += `---------------> ${key}<br>`;
@@ -28,4 +73,53 @@ for(let key in studente)
 
 message+='</div>';
 
+/* stampo sullo schemrmo */
 propertyElement.innerHTML = message;
+
+/* _______________________________ 4 _______________________________ */
+
+/* prendo il numero di studenti */
+const numberStudents  = studenti.length;
+
+message = "<h5> 4. Gli studenti sono:</h3><br>";
+
+    for(let i=0;i < numberStudents;i++)
+    {
+        const currentStudent =  studenti[i];
+
+        message += `Nome:<strong> ${currentStudent.nome}</strong><br>Cognome: <strong> ${currentStudent.cognome} </strong> <br>`;
+        message +='-------------------------------------------------<br>';
+    }
+
+    studentsElement.innerHTML = message;
+
+
+insertElement.addEventListener('click',()=>{
+
+        addstudent =
+        {
+            nome : '',
+            cognome : '',
+            eta : 0    
+        }
+
+    addstudent.nome = nameElement.value;
+    addstudent.cognome = surnameElement.value;
+    addstudent.eta = etaElement.value;
+
+    studenti.push(addstudent);
+
+    console.table(studenti);
+
+    addedTextStudent = `Nome:<strong> ${addstudent.nome}</strong><br>Cognome: <strong> ${addstudent.cognome} </strong> <br>`;
+    addedTextStudent +='-------------------------------------------------<br>';
+    studentsElement.innerHTML += addedTextStudent;
+
+});
+
+
+
+
+
+
+
